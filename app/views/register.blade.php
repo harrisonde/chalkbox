ba145193c8c2f91755dbc1d257406772d0d2c2f2
Harrison DeStefano <harrison.destefano@gmail.com>
2014-07-22 19:41:14 -0400

    updated registration.blade logic

--- app/views/register.blade.php
+++ app/views/register.blade.php
@@ -12,30 +12,49 @@
 	
 		{{ $query['response'][0][200] }}			
 
-	@endif
 	
-	@if (isset( $query['response'][0][409]) )
+	@elseif (isset( $query['response'][0][409]) )
 	
 		{{ $query['response'][0][409] }}			
 
+		{{ Form::open() }}
+		
+			{{ Form::label('email', 'Email: (email@somewhere.com)') }}
+			
+			{{ Form::text('email') }}
+			
+			{{ Form::label('username', 'Username:') }}
+			
+			{{ Form::text('username') }}
+			
+			{{ Form::label('password', 'Password:') }}
+			
+			{{ Form::text('password') }}
+			
+			{{ Form::submit('Register') }}
+	
+		{{ Form::close() }}
+		
+	@else
+		
+		{{ Form::open() }}
+		
+			{{ Form::label('email', 'Email: (email@somewhere.com)') }}
+			
+			{{ Form::text('email') }}
+			
+			{{ Form::label('username', 'Username:') }}
+			
+			{{ Form::text('username') }}
+			
+			{{ Form::label('password', 'Password:') }}
+			
+			{{ Form::text('password') }}
+			
+			{{ Form::submit('Register') }}
+	
+		{{ Form::close() }}
+	
 	@endif
 	
-	{{ Form::open() }}
-		
-		{{ Form::label('email', 'Email: (email@somewhere.com)') }}
-		
-		{{ Form::text('email') }}
-		
-		{{ Form::label('username', 'Username:') }}
-		
-		{{ Form::text('username') }}
-		
-		{{ Form::label('password', 'Password:') }}
-		
-		{{ Form::text('password') }}
-		
-		{{ Form::submit('Register') }}
-	
-	{{ Form::close() }}
-
 @stop 

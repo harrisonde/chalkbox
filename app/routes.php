1ff5800c4f174f276ab181672aedbe9017108535
Harrison DeStefano <harrison.destefano@gmail.com>
2014-07-23 08:14:56 -0400

    configure email for local env

--- app/routes.php
+++ app/routes.php
@@ -27,3 +27,16 @@ Route::resource('search', 'SearchController');
 Route::resource('signin', 'SignInController');
 
 Route::resource('signout', 'SignOutController');
+
+Route::get('mail', function(){
+
+	$name = 'index'; # can be 'html.view' or 'text.view'.
+	$data = array('boom' => 'city!');  #data that you want to make available to your email, same as passing data to a view.
+	//$message =  # allows you to specify a couple of different things on the email such as who to send it to and the subject of the message
+	
+	Mail::send($name, $data, function($message) #Mail::send(array('html.view', 'text.view'), $data, $callback);
+	{
+    	$message->to('harrison.destefano@gmail.com', 'Harrison')
+    	->subject('Welcome!');
+	});
+});

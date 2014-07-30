6cc0dc6090900885fbb710d4e566864eb0c4edc9
Harrison DeStefano <harrison.destefano@gmail.com>
2014-07-30 16:44:23 -0400

    project.search blade structure updated.

--- app/views/search.blade.php
+++ app/views/search.blade.php
@@ -2,37 +2,59 @@
 
 @section('body')
 	
-	<h2>Search Results.</h2>
+	
+	{{-- Page Description. ------------------------}}
+	
+	<div class="chalk-lines-100">
+	
+		<h1>Search</h1>
+	
+	</div>
+	
+	<div class="chalk-lines-75">			
 		
-	{{-- Search Form. ------------------------}}
-	
-	{{ Form::open( array('url' => 'search')) }}
-	
-			{{ Form::text('search', Input::old('search'),  array('placeholder'=>'Project Name')) }}
-	
-			{{ Form::submit('Search') }}
-	
-		{{ Form::close() }}
-
+		{{-- Section title. ------------------------}}
 		
-	{{-- Search Results. ------------------------}}
-	
-	@if (isset($query_results))
-	
-		@foreach($query_results as $node) 
+		@if (isset($query_results))
 			
-			@if (isset($node['name']))
+			{{-- Search results. ------------------------}}
 				
-				<a href="/projects/{{ $node['id'] }}">{{ $node['name'] }}</a>
+			<h2>{{ sizeof($query_results) }} Result(s)</h2>	
+				
+			<ul class="result">
 			
-			@else
+			@foreach($query_results as $node) 
+				
+				@if (isset($node['name']))
+					
+					<li><a href="/projects/{{ $node['id'] }}">{{ $node['name'] }}</a></li>
+				
+				@else
+				
+					<p>{{ $node }}</p>	
+				
+				@endif
+		
+			@endforeach
 			
-				<p>{{ $node }}</p>	
+			</ul>
 			
-			@endif
+		@endif
 	
-		@endforeach
+	</div>
 	
-	@endif
+	<div class="chalk-lines-25">
+			
+		{{-- Search Form. ------------------------}}
+		
+		{{ Form::open( array('url' => 'search')) }}
+		
+				{{ Form::text('search', Input::old('search'),  array('placeholder'=>'Project Name')) }}
+		
+				{{ Form::submit('Search') }}
+		
+			{{ Form::close() }}
+	
+	</div>
 
 @stop 	

69a09cfd420ede89aae5c75d662d5c6703af06ee
Harrison DeStefano <harrison.destefano@gmail.com>
2014-07-30 12:58:04 -0400

    projects.blade create new link moved into html5 button with onclick event

--- app/views/projects.blade.php
+++ app/views/projects.blade.php
@@ -2,29 +2,56 @@
 
 @section('body')
 
-	<h1>Projects.</h1>		
+	<div class="chalk-lines-50">
 	
-	{{-- Create Project. ------------------------}}		
+		<h1>Projects</h1>		
 		
-		<div class="create">
-		
-			{{link_to('projects/create', '+ New Project')}}	
-	
-		</div>
-	
-	{{-- Project  List. ------------------------}}
-	
-	@if (isset($query))
-		<div class="projectList">
-		
-			<ul>
-			@foreach($query as $node) 
+		{{-- Create Project. ------------------------}}		
 			
-			<li><a href="/projects/{{ $node['id'] }}">{{ $node['name'] }}</a></li>
+			<div class="create">
+				
+				<button onClick="window.location.href='/projects/create'">+ New Project </button>
+				
+			</div>
+		
+		{{-- Project  List. ------------------------}}
+		
+		@if (isset($query))
+			<div class="projectList">
 			
-			@endforeach
-			</ul>
+				<ul>
+				@foreach($query as $node) 
+				
+				<li><a href="/projects/{{ $node['id'] }}">{{ $node['name'] }}</a></li>
+				
+				@endforeach
+				</ul>
+			
+			</div>
+	
 		</div>
 		
 	@endif
+
+	<div class="chalk-lines-50">
+	
+		<h1> Project Feed </h1>
+		
+		<ul>
+			
+			<li>Something</li>
+			
+			<li>Else about</li>
+			
+			<li>some sort of</li>
+			
+			<li>action(s) the user has</li>
+			
+			<li> taken on any number of</li>
+			
+			<li> the projects they own</li>
+			
+		</ul>	
+	
+	</div>
 @stop 	

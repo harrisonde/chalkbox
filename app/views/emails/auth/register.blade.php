b20d7d9779a64ca8bffaf35b77524d6a9adb8afc
Harrison DeStefano <harrison.destefano@gmail.com>
2014-08-04 15:06:22 -0400

    email template lan change

--- app/views/emails/auth/register.blade.php
+++ app/views/emails/auth/register.blade.php
@@ -6,6 +6,6 @@
 	<body>
 		<p>Hi  {{ $email }},</p>
 		<p>Welcome To Chalkbox! Thank you for registering! We're excited to have you onboard.</p>
-		<h4>Manage your projects like a creative - not an accountant</h4>
+		<h4>Manage your projects like a web artisan - not an accountant.</h4>
 	</body>
 </html>
